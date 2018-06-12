e084c92b0f7abc05277e925716287a2e09116986
Eiji Onchi <onchi.eiji@gmail.com>
2018-06-12 21:54:42 +0900

    Dirty eye tracker

--- camera.py
+++ camera.py
@@ -7,26 +7,73 @@ import logging
 import cv2
 import numpy as np
 import time
+from roi import ROI
 
 def main():
+    camera = cv2.VideoCapture(1)
+
     abs_path = os.path.abspath(os.path.dirname(__file__))
     path = os.path.join(abs_path, 'lbp/frontal.xml')
     faceCascade = cv2.CascadeClassifier(path)
+
+    path = os.path.join(abs_path, 'lbp/right.xml')
+    rightCascade = cv2.CascadeClassifier(path)
+
+    path = os.path.join(abs_path, 'lbp/left.xml')
+    leftCascade = cv2.CascadeClassifier(path)
+
+    face_roi = ROI(1000,1000)
+
     if faceCascade.empty():
         raise Exception("Face Classifier not found!", path)
 
-    camera = cv2.VideoCapture(1)
+    
 
     while True:
         ret_val, img = camera.read()
 
+        roi = face_roi.get_roi(img)
+        min_size, max_size = face_roi.get_size()
+
         faces = []
-        faces = faceCascade.detectMultiScale(img, 1.1, 2, 0|cv2.CASCADE_SCALE_IMAGE, (0,0), (0,0))
+        faces = faceCascade.detectMultiScale(roi, 1.1, 2, 0|cv2.CASCADE_SCALE_IMAGE, min_size, max_size)
 
-        if len(faces) != 0:
+        if len(faces) == 0:
+            if face_roi.is_enabled():
+                face_roi.enable(False)
+        elif len(faces) != 0:
             (x, y, w, h) = get_biggest(faces)
-            img = cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255))
+            face_roi.set_roi(x, y, w, h)
+            pt1, pt2 = face_roi.get_rect()
+            img = cv2.rectangle(img, pt1, pt2, (0, 0, 255))
+        
+        eyes = []
+        eyes = rightCascade.detectMultiScale(roi, 1.1, 2, 0|cv2.CASCADE_SCALE_IMAGE, (0,0), (0,0))
 
+        if len(eyes) != 0:
+            (x, y, w, h) = get_biggest(eyes)
+            x /= face_roi._scale_factor
+            y /= face_roi._scale_factor
+            w /= face_roi._scale_factor
+            h /= face_roi._scale_factor
+            pt1, pt2 = face_roi.get_rect()
+            ps = (int(x) + pt1[0], int(y) + pt1[1])
+            pf = (int(x + pt1[0] + w), int(y + pt1[1] + h))
+            img = cv2.rectangle(img, ps, pf, (255, 0, 0))
+            
+        eyes = leftCascade.detectMultiScale(roi, 1.1, 2, 0|cv2.CASCADE_SCALE_IMAGE, (0,0), (0,0))
+
+        if len(eyes) != 0:
+            (x, y, w, h) = get_biggest(eyes)
+            x /= face_roi._scale_factor
+            y /= face_roi._scale_factor
+            w /= face_roi._scale_factor
+            h /= face_roi._scale_factor
+            pt1, pt2 = face_roi.get_rect()
+            ps = (int(x) + pt1[0], int(y) + pt1[1])
+            pf = (int(x + pt1[0] + w), int(y + pt1[1] + h))
+            img = cv2.rectangle(img, ps, pf, (0, 255, 0))
+        
         cv2.imshow("test", img)
         if cv2.waitKey(1) == 27:
             break
---
+++ roi.py
@@ -0,0 +1,133 @@
+import cv2
+
+class ROI(object):
+    def __init__(self, max_width, max_height):
+        self.is_detected = False
+        self._margin = 0.8 # percent
+        self._margin_low = 1 - self._margin
+        self._margin_high = 1 + self._margin
+        self._c_margin = 1 + self._margin / 2
+        self._width = 0
+        
+        self._max_width = max_width
+        self._max_height = max_height
+        self._mid_max_width = max_width / 2
+        self._mid_max_height = max_height / 2
+
+        self.DEFAULT_SCALE_FACTOR = 0.8
+        self._scale_factor = self.DEFAULT_SCALE_FACTOR
+        
+
+        self._x1 = 0
+        self._x2 = 0
+        self._y1 = 0
+        self._y2 = 0
+
+    def set_roi(self, x, y, width, height):
+        # Take into account the offset of the previous ROI, if available
+        # Take into account the scale factor if it is a new face
+        if self.is_detected:
+            _x = self._x1 + x / self._scale_factor
+            _y = self._y1 + y / self._scale_factor
+            self._width = width / self._scale_factor
+        else:
+            _x = x / self._scale_factor
+            _y = y / self._scale_factor
+            self._width = width / self._scale_factor
+
+        wc = self._c_margin * self._width
+        w_by_c_minus_1 = wc - self._width
+
+        # Increase the boundaries by the margin
+        self._x1 = int(_x - w_by_c_minus_1)
+        if self._x1 < 0:
+            self._x1 = 0
+        self._y1 = int(_y - w_by_c_minus_1)
+        if self._y1 < 0:
+            self._y1 = 0
+
+        # Assume square shape for speed
+        self._x2 = int(_x + wc)
+        if self._x2 > self._max_width:
+            self._x2 = self._max_width
+
+        self._y2 = int(_y + wc)
+        if self._y2 > self._max_height:
+            self._y2 = self._max_height
+
+        # Enable the flag for this ROI
+        self.is_detected = True
+
+    def get_roi(self, image):
+        if self.is_detected:
+            img = image[self._y1:self._y2, self._x1:self._x2]
+            self._scale_factor = 120 / self._width
+            return cv2.resize(img, (0, 0), fx=self._scale_factor, fy=self._scale_factor)
+        else:
+            return cv2.resize(image, (0, 0), fx=self._scale_factor, fy=self._scale_factor)
+
+    def get_size(self):
+        if self.is_detected:
+            # Assuming a square shape for speed
+            min_size = int(self._margin_low*self._width)
+            max_size = int(self._margin_high*self._width)
+
+            return (min_size, min_size), (max_size, max_size)
+        else:
+            return (0, 0), (0, 0)
+
+    def enable(self, boolean):
+        if boolean == False:
+            self._scale_factor = self.DEFAULT_SCALE_FACTOR
+        self.is_detected = boolean;
+
+    def is_enabled(self):
+        return self.is_detected
+
+    def get_rect(self):
+        if self.is_detected:
+            return (self._x1, self._y1), (self._x2, self._y2)
+        else:
+            return (0, 0), (0, 0)
+
+    def center(self):
+        x = (self._x1 + self._x2) / 2
+        y = (self._y1 + self._y2) / 2
+
+        return x, y
+    
+    def from_center(self):
+        if self.is_detected:
+            x = self._mid_max_width - (self._x1 + self._x2) / 2
+            y = self._mid_max_height - (self._y1 + self._y2) / 2
+            return x, y
+        
+        return 0, 0
+
+    def set_margin(self, margin):
+        self._margin = margin
+        self._margin_low = 1 - margin
+        self._margin_high = 1 + margin
+        self._c_margin = 1 + self._margin / 2
+
+    def set_max(self, max_width, max_height):
+        self._max_width = max_width
+        self._max_height = max_height
+        self._mid_max_width = max_width / 2
+        self._mid_max_height = max_height / 2
+
+    def offset(self, x, y):
+        self._x1 += x
+        self._x2 += x
+        self._y1 += y
+        self._y2 += y
+
+        if self._x1 < 0:
+            self._x1 = 0
+        if self._y1 < 0:
+            self._y1 = 0
+
+        if self._x2 > self._max_width:
+            self._x2 = self._max_width
+        if self._y2 > self._max_height:
+            self._y2 = self._max_height
