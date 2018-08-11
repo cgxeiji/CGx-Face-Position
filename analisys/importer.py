d4f176dce9777e088508ccdaed5ef2a23a29c72e
Eiji Onchi <onchi.eiji@gmail.com>
2018-08-11 17:06:45 +0900

    Added speed graph and overlay of Pose Safe location

--- analisys/importer.py
+++ analisys/importer.py
@@ -11,6 +11,8 @@ from matplotlib.widgets import Slider, Button, RadioButtons
 import operator
 import Tkinter as tk
 import tkFileDialog as filedialog
+import matplotlib.collections as collections
+
 
 class Smoother:
     def __init__(self, size=3):
@@ -29,6 +31,7 @@ class Smoother:
     def value(self):
         return sum(self.buffer) / len(self.buffer)
 
+
 def main():
     try:
         face_frames = []
@@ -38,9 +41,9 @@ def main():
         root = tk.Tk()
         root.withdraw()
 
-        smooth_distance = Smoother(20)
-        smooth_angle = Smoother(20)
-        smooth_speed = Smoother(20)
+        smooth_distance = Smoother(50)
+        smooth_angle = Smoother(50)
+        smooth_speed = Smoother(50)
 
         filepath = ''
 
@@ -61,19 +64,22 @@ def main():
                     sections = line.split('->')
                     if len(sections) > 3:
                         if 'face_data' in sections[2]:
-                            face_time = datetime.strptime(sections[1], "%Y-%m-%d %H:%M:%S,%f")
+                            face_time = datetime.strptime(
+                                sections[1], "%Y-%m-%d %H:%M:%S,%f")
                             data = sections[3].split(', ')
                             data.append(face_time)
                             data.append('face')
                             face_frames.append(data)
                         elif 'monitor_data' in sections[2]:
-                            monitor_time = datetime.strptime(sections[1], "%Y-%m-%d %H:%M:%S,%f")
+                            monitor_time = datetime.strptime(
+                                sections[1], "%Y-%m-%d %H:%M:%S,%f")
                             data = sections[3].split(',')
                             data.append(monitor_time)
                             data.append('monitor')
                             monitor_frames.append(data)
                         elif 'motion' in sections[2]:
-                            motion_time = datetime.strptime(sections[1], "%Y-%m-%d %H:%M:%S,%f")
+                            motion_time = datetime.strptime(
+                                sections[1], "%Y-%m-%d %H:%M:%S,%f")
                             data = [str(sections[3])]
                             data.append(motion_time)
                             data.append('motion')
@@ -81,33 +87,55 @@ def main():
 
         print('data loaded\nProcessing data...')
 
-        with open('face_data.csv', 'wb') as csv_file:
-            writer = csv.writer(csv_file)
-            labels = ['timestamp (s)', 'x', 'y', 'z', 'angle', 'zone', 'time (s)']
-            data = []
-            start_time = None
-            for frame in face_frames:
-                if start_time == None:
-                    start_time = frame[len(frame) - 2]
-                timestamp = (frame[len(frame) - 2] - start_time).total_seconds()
-                datum = []
-                datum.append(timestamp)
-                datum.extend(frame[0:6])
-                data.append(datum)
-            
-            dump(writer, labels, data)
+        # with open('face_data.csv', 'wb') as csv_file:
+        #     writer = csv.writer(csv_file)
+        #     labels = ['timestamp (s)', 'x', 'y', 'z',
+        #               'angle', 'zone', 'time (s)']
+        #     data = []
+        #     start_time = None
+        #     for frame in face_frames:
+        #         if start_time == None:
+        #             start_time = frame[len(frame) - 2]
+        #         timestamp = (frame[len(frame) - 2] -
+        #                      start_time).total_seconds()
+        #         datum = []
+        #         datum.append(timestamp)
+        #         datum.extend(frame[0:6])
+        #         data.append(datum)
 
-        print('... created *.csv file')
+        #     dump(writer, labels, data)
+
+        # print('... created *.csv file')
+
+        face_data = []
+        start_time = None
+        for frame in face_frames:
+            if start_time == None:
+                start_time = frame[len(frame) - 2]
+            timestamp = (frame[len(frame) - 2] -
+                         start_time).total_seconds()
+            datum = []
+            datum.append(timestamp)
+            datum.extend(frame[0:6])
+            face_data.append(datum)
 
         monitor_data = []
-        monitor_labels = {'Default Position':0, 'Default Fast':-1, 'Move forward':-2, 'Move upward':-3, 'Move left':-4, 'Move right':-5, 'Turn clockwise':-6, 'Turn counter clockwise':-7}
-        start_time = None
+        monitor_labels = {
+            'Default Position': 0,
+            'Default Fast': -1,
+            'Move forward': -2,
+            'Move upward': -3,
+            'Move left': -4,
+            'Move right': -5,
+            'Turn clockwise': -6,
+            'Turn counter clockwise': -7}
         for frame in monitor_motion:
             if not monitor_labels.has_key(frame[0]):
                 continue
             if start_time == None:
                 start_time = frame[len(frame) - 2]
-            timestamp = (frame[len(frame) - 2] - start_time).total_seconds()
+            timestamp = (frame[len(frame) - 2]
+                         - start_time).total_seconds()
             datum = []
             datum.append(timestamp)
             datum.append(frame[0])
@@ -126,30 +154,37 @@ def main():
         zones = {}
         prev_pos = np.array((0, 0, 0))
         prev_time = 0
-        for datum in data:
+        for datum in face_data:
             time_data.append(float(datum[0]))
             x.append(float(datum[1]))
             y.append(float(datum[2]))
             z.append(float(datum[3]))
-            current_pos = np.array((float(datum[1]), float(datum[2]), float(datum[3])))
+            current_pos = np.array(
+                (float(datum[1]), float(datum[2]), float(datum[3])))
             dist = np.linalg.norm(current_pos - prev_pos)
-            sp = dist/(float(datum[0]) - prev_time) if (float(datum[0]) - prev_time) != 0 else 0
+            sp = dist / (float(datum[0]) - prev_time) \
+                if (float(datum[0]) - prev_time) != 0 \
+                else 0
+
             smooth_speed.input(sp if sp < 400 else 0)
             speed.append(smooth_speed.value())
-            smooth_distance.input(np.linalg.norm(current_pos - np.array((0, 0, 0))))
+            smooth_distance.input(np.linalg.norm(
+                current_pos - np.array((0, 0, 0))))
             distance.append(smooth_distance.value())
             smooth_angle.input(float(datum[4]))
             angle_data.append(smooth_angle.value())
             prev_pos = current_pos
             prev_time = float(datum[0])
-        
-        for i in range(1, len(data)):
-            if data[i][5] not in zones:
-                zones[data[i][5]] = 0.0
 
-            if data[i][5] == data[i-1][5]:
-                if float(data[i][6]) != -1:
-                    zones[data[i][5]] += (float(data[i][6]) - float(data[i-1][6]))
+        for i in range(1, len(face_data)):
+            if face_data[i][5] not in zones:
+                zones[face_data[i][5]] = 0.0
+
+            if face_data[i][5] == face_data[i-1][5]:
+                if float(face_data[i][6]) != -1:
+                    zones[face_data[i][5]] += (
+                        float(face_data[i][6])
+                        - float(face_data[i-1][6]))
 
         print('... face zones')
 
@@ -190,17 +225,39 @@ def main():
 
         print('... computing times')
 
-        _color_dict = {'Pose Safe':'green', '':'white','CGx Bug':'white', 'Face Lost':'lightgrey', 'Leaning right':'purple', 'Leaning left':'navy', 'Lean backward':'salmon', 'Lean forward':'saddlebrown', 'Head tilt left':'orchid', 'Head tilt right':'royalblue'}
-        _y_dict = {'Pose Safe':0, '':0,'CGx Bug':0, 'Face Lost':1, 'Leaning right':2, 'Leaning left':3, 'Lean backward':4, 'Lean forward':5, 'Head tilt left':6, 'Head tilt right':7}
+        _color_dict = {
+            'Pose Safe': 'green',
+            '': 'white',
+                'CGx Bug': 'white',
+                'Face Lost': 'lightgrey',
+                'Leaning right': 'purple',
+                'Leaning left': 'navy',
+                'Lean backward': 'salmon',
+                'Lean forward': 'saddlebrown',
+                'Head tilt left': 'orchid',
+                'Head tilt right': 'royalblue'}
+        _y_dict = {
+            'Pose Safe': 0,
+            '': 0,
+            'CGx Bug': 0,
+            'Face Lost': 1,
+            'Leaning right': 2,
+            'Leaning left': 3,
+            'Lean backward': 4,
+            'Lean forward': 5,
+            'Head tilt left': 6,
+            'Head tilt right': 7}
 
         #_color_dict = {'Pose Safe':'green', '':'white','CGx Bug':'white', 'Face Lost':'white', 'Leaning right':'saddlebrown', 'Leaning left':'saddlebrown', 'Lean backward':'saddlebrown', 'Lean forward':'saddlebrown', 'Head tilt left':'saddlebrown', 'Head tilt right':'saddlebrown'}
 
-        zones_fig, zones_ax = plt.subplots(figsize=(6, 3), subplot_kw=dict(aspect="equal"))
+        zones_fig, zones_ax = plt.subplots(
+            figsize=(6, 3), subplot_kw=dict(aspect="equal"))
 
         zones_clean = {}
         for key, value in zones.iteritems():
             if key not in ["Face Lost", '', "CGx Bug"]:
-                zones_clean["{}\n{:.0f}:{:02.0f}".format(key, value/60, value%60)] = value
+                zones_clean["{}\n{:.0f}:{:02.0f}".format(
+                    key, value/60, value % 60)] = value
 
         zones_show = {}
         recipe = []
@@ -209,9 +266,11 @@ def main():
         while len(zones_clean) > 0:
             key = ''
             if order_flag:
-                key = max(zones_clean.iteritems(), key=operator.itemgetter(1))[0]
+                key = max(zones_clean.iteritems(),
+                          key=operator.itemgetter(1))[0]
             else:
-                key = min(zones_clean.iteritems(), key=operator.itemgetter(1))[0]
+                key = min(zones_clean.iteritems(),
+                          key=operator.itemgetter(1))[0]
             value = zones_clean.pop(key)
             recipe.append(key)
             _data.append(value)
@@ -219,11 +278,21 @@ def main():
 
         print('... plotting')
 
-        wedges, texts = zones_ax.pie(_data, wedgeprops=dict(width=0.5), startangle=-89)
+        wedges, texts = zones_ax.pie(
+            _data, wedgeprops=dict(width=0.5), startangle=-89)
 
-        bbox_props = dict(boxstyle="square,pad=0.3", fc="w", ec="k", lw=0.72)
-        kw = dict(xycoords='data', textcoords='data', arrowprops=dict(arrowstyle="-"),
-                bbox=bbox_props, zorder=0, va="center")
+        bbox_props = dict(
+            boxstyle="square,pad=0.3",
+            fc="w",
+            ec="k",
+            lw=0.72)
+        kw = dict(
+            xycoords='data',
+            textcoords='data',
+            arrowprops=dict(arrowstyle="-"),
+            bbox=bbox_props,
+            zorder=0,
+            va="center")
 
         for i, p in enumerate(wedges):
             ang = (p.theta2 - p.theta1)/2. + p.theta1
@@ -232,15 +301,21 @@ def main():
             horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(_x))]
             connectionstyle = "angle,angleA=0,angleB={}".format(ang)
             kw["arrowprops"].update({"connectionstyle": connectionstyle})
-            zones_ax.annotate(recipe[i], xy=(_x, _y), xytext=(1.35*np.sign(_x), 1.4*_y), horizontalalignment=horizontalalignment, **kw)
+            zones_ax.annotate(recipe[i], xy=(_x, _y), xytext=(
+                1.35*np.sign(_x), 1.4*_y), horizontalalignment=horizontalalignment, **kw)
 
         zones_ax.set_title("Pose Distribution")
         #zones_ax.legend(wedges, recipe)
-        
+
+        face_in_safe = []
         _bar_text = []
         _bar_start = []
         _bar_end = []
-        for datum in data:
+        for datum in face_data:
+            if datum[5] == 'Pose Safe':
+                face_in_safe.append(1)
+            else:
+                face_in_safe.append(0)
             if len(_bar_text) > 0:
                 if datum[5] not in 'Face Lost' and datum[5] not in _bar_text[len(_bar_text) - 1]:
                     _bar_end.append(datum[0])
@@ -250,30 +325,44 @@ def main():
                 _bar_text.append(datum[5])
                 _bar_start.append(datum[0])
 
-        _bar_end.append(data[len(data) - 1][0])
+        _bar_end.append(face_data[len(face_data) - 1][0])
 
-        
-                    
-        plt.figure()
-        fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True)
-        ax1.set_title(filepath)
+        fig, ax = plt.subplots()
+        ax.set_title(filepath)
         for i in range(len(_bar_text)):
             color = _color_dict[_bar_text[i]]
-            # ax1.hlines(1, _bar_start[i], _bar_end[i], colors=color, lw=40, label=_bar_text[i])
-            ax1.hlines(_y_dict[_bar_text[i]], _bar_start[i], _bar_end[i], colors=color, lw=20)
+            # ax.hlines(1, _bar_start[i], _bar_end[i], colors=color, lw=40, label=_bar_text[i])
+            ax.hlines(_y_dict[_bar_text[i]], _bar_start[i],
+                      _bar_end[i], colors=color, lw=20)
             # plt.text((_bar_start[i] + _bar_end[i]) / 2, 1.01, _bar_text[i], ha='center')
 
         # labels = ['Monitor', 'Pose Safe', 'Leaning right', 'Leaning left', 'Lean backward', 'Lean forward', 'Head tilt left', 'Head tilt right']
-        
+
         # ax1.set_yticklabels(labels)
 
-        _color_dict = {'Default Position':'green', 'Default Fast':'lightgrey', 'Move forward':'purple', 'Move upward':'navy', 'Move left':'salmon', 'Move right':'saddlebrown', 'Turn clockwise':'orchid', 'Turn counter clockwise':'royalblue'}
-        _y_dict = {'Default Position':-3, 'Default Fast':-4, 'Move forward':-5, 'Move upward':-6, 'Move left':-5, 'Move right':-10, 'Turn clockwise':-9, 'Turn counter clockwise':-10}
+        _color_dict = {
+            'Default Position': 'green',
+            'Default Fast': 'lightgrey',
+            'Move forward': 'purple',
+            'Move upward': 'navy',
+            'Move left': 'salmon',
+            'Move right': 'saddlebrown',
+            'Turn clockwise': 'orchid',
+            'Turn counter clockwise': 'royalblue'}
+        _y_dict = {
+            'Default Position': -3,
+            'Default Fast': -4,
+            'Move forward': -5,
+            'Move upward': -6,
+            'Move left': -5,
+            'Move right': -10,
+            'Turn clockwise': -9,
+            'Turn counter clockwise': -10}
 
         _monitor_text = []
         _monitor_start = []
         _monitor_end = []
-        
+
         for datum in monitor_data:
             if len(_monitor_text) > 0:
                 if datum[1] not in _monitor_text[len(_monitor_text) - 1]:
@@ -288,19 +377,42 @@ def main():
 
         for i in range(len(_monitor_text)):
             color = _color_dict[_monitor_text[i]]
-            ax1.hlines(_y_dict[_monitor_text[i]], _monitor_start[i], _monitor_end[i], colors=color, lw = 20)
-
+            ax.hlines(_y_dict[_monitor_text[i]], _monitor_start[i],
+                      _monitor_end[i], colors=color, lw=20)
 
         # ax1.ylim(0.95, 1.05)
-        plt.legend()
+        # plt.legend()
 
-        plt.figure()
+        fig, ax = plt.subplots()
+        ax.set_title("Distance")
 
-        plt.plot(time_data, distance, 'g')
-        # plt.set_ylim(-10, 100)
-        plt.figure()
-        plt.plot(time_data, angle_data, 'r')
-        # plt.set_ylim(-90, 90)
+        ax.plot(time_data, distance, 'black', linewidth=2)
+        collection = collections.BrokenBarHCollection.span_where(
+            np.array(time_data), ymin=-10, ymax=40,
+            where=np.array(face_in_safe) > 0,
+            facecolor="green", alpha=0.5, linewidths=0)
+        ax.add_collection(collection)
+        ax.set_ylim(-10, 40)
+
+        fig, ax = plt.subplots()
+        ax.set_title("Angle")
+        ax.plot(time_data, angle_data, 'black', linewidth=2)
+        collection = collections.BrokenBarHCollection.span_where(
+            np.array(time_data), ymin=-90, ymax=90,
+            where=np.array(face_in_safe) > 0,
+            facecolor="green", alpha=0.5, linewidths=0)
+        ax.add_collection(collection)
+        ax.set_ylim(-90, 90)
+
+        fig, ax = plt.subplots()
+        ax.set_title("Speed")
+        ax.plot(time_data, speed, 'black', linewidth=2)
+        collection = collections.BrokenBarHCollection.span_where(
+            np.array(time_data), ymin=-10, ymax=40,
+            where=np.array(face_in_safe) > 0,
+            facecolor="green", alpha=0.5, linewidths=0)
+        ax.add_collection(collection)
+        ax.set_ylim(-10, 40)
 
         print('done!')
 
