4012c6db09c4cda597c3b888b635b7afa03caba0
Eiji Onchi <onchi.eiji@gmail.com>
2018-09-04 00:22:07 +0900

    Added plotter options

--- json_parser.py
+++ json_parser.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+import matplotlib.pyplot as plt
 import traceback
 import json
 import argparse
@@ -12,6 +13,9 @@ parser = argparse.ArgumentParser(
     description='Parse json points generated from Open Pose')
 parser.add_argument("-f", "--folder", dest="folderpath", required=True,
                     help="Specify the filepath of the log file to process")
+parser.add_argument("-p", "--plot", dest="plot_enabled",
+                    action="store_true",
+                    help="Plot data")
 
 
 def main():
@@ -38,6 +42,11 @@ def main():
     baseY = None
     baseZ = None
 
+    face_x = []
+    face_y = []
+    face_z = []
+    face_a = []
+
     for eyes in eyes_list:
         rx = eyes[0, 0]
         ry = eyes[0, 1]
@@ -62,6 +71,10 @@ def main():
         _z -= baseZ
 
         face.append((_x, _y, _z, _angle))
+        face_x.append(_x)
+        face_y.append(_y)
+        face_z.append(_z)
+        face_a.append(_angle)
 
     # pprint.pprint(eyes_list)
     # pprint.pprint(face)
@@ -69,6 +82,38 @@ def main():
     save_path = "{}/{}.cgx".format(folderpath, "face")
     pickle.dump(face, open(save_path, "wb"))
     print("Data saved at: {}".format(save_path))
+
+    if args.plot_enabled:
+        fig, ax = plt.subplots()
+        time_data = [i*0.5 for i in range(len(face))]
+
+        ax.plot(time_data, face_x,
+                "purple", linewidth=1)
+        ax.plot(time_data, face_y,
+                "green", linewidth=1)
+        ax.plot(time_data, face_z,
+                "blue", linewidth=1)
+
+        ax.set_ylim(-10, 10)
+        ax.set_xlim(
+            left=0,
+            right=500)
+
+        ax2 = ax.twinx()
+        ax2.set_ylabel("Angle (degrees)",
+                       color='red')
+        ax2.tick_params(
+            axis='y', labelcolor='red')
+
+        ax2.plot(time_data, face_a,
+                 'red', linewidth=1)
+
+        ax2.set_ylim(-20, 20)
+        ax2.set_xlim(
+            left=0,
+            right=500)
+
+        plt.show()
 
 
 if __name__ == "__main__":
