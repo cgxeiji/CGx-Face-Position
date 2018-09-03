2a44ce90cfad9374b0a1dcd583426af87807a1ef
Eiji Onchi <onchi.eiji@gmail.com>
2018-09-03 23:36:34 +0900

    Transform json to xyz angle data

--- json_parser.py
+++ json_parser.py
@@ -31,7 +31,39 @@ def main():
             eyes = data_array[14:16, :2]
             eyes_list.append(eyes)
 
+    face = []
+
+    baseX = None
+    baseY = None
+    baseZ = None
+
+    for eyes in eyes_list:
+        rx = eyes[0, 0]
+        ry = eyes[0, 1]
+        lx = eyes[1, 0]
+        ly = eyes[1, 1]
+
+        _angle = (np.arctan2(ly - ry, lx - rx) * 180.0 / np.pi)
+        _d = np.sqrt(np.power(ly - ry, 2) + np.power(lx - rx, 2))
+
+        _z = (0.0043 * np.power(_d, 2) - 1.2678 * _d + 116.02)
+
+        _x = (rx + lx) / 2
+        _y = (ry + ly) / 2
+
+        if baseX is None:
+            baseX = _x
+            baseY = _y
+            baseZ = _z
+
+        _x -= baseX
+        _y -= baseY
+        _z -= baseZ
+
+        face.append((_x, _y, _z, _angle))
+
     pprint.pprint(eyes_list)
+    pprint.pprint(face)
 
 
 if __name__ == "__main__":
